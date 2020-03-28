3ef44fdfb81b6531dc8ef926c1a31dec323b71bd
arsen204 <arsen.vardumyan1@gmail.com>
2020-03-28 19:38:34 +0300

    optimized DaysInCurYear function...

--- DBDate.cpp
+++ DBDate.cpp
@@ -1,136 +1,138 @@
-#include "DBDate.h"
+#include "dbmsLib.h"
 
-int DBDate::GetDay() const {
-    return m_day;
-}
-
-int DBDate::GetMonth() const {
-    return m_month;
-}
-
-int DBDate::GetYear() const {
-    return m_year;
-}
-
-DBDate::DBDate(const string &date) {
-    m_day = stoi(date.substr(0, 2));
-    m_month = stoi(date.substr(3, 2));
-    m_year = stoi(date.substr(6, 4));
-}
-
-string DateToStr(const DBDate &date) {
-    return to_string(date.m_day) + to_string(date.m_month) + to_string(date.m_year);
-}
-
-ostream &operator<<(ostream &out, const DBDate &date) {
-    if (date.m_day < 10) out << "0";
-    out << date.m_day << ".";
-    if (date.m_month < 10) out << "0";
-    out << date.m_month << ".";
-    out<<date.m_year;
-    return out;
-}
-
-bool DBDate::IsLeapYear(int year) {
-    return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-}
-
-int DBDate::GetDaysInMonth(int month, int year) {
-    if (month < 8 && month % 2 == 1) return 31;
-    if (month >= 8 && month % 2 == 0) return 31;
-    if (month == 2) return DBDate::IsLeapYear(year) ? 29 : 28;
-    return 30;
-}
-
-bool DBDate::operator<(const DBDate &date) const {
-    if (m_year != date.m_year) return m_year < date.m_year;
-    if (m_month != date.m_month) return  m_month < date.m_month;
-    return m_day < date.m_day;
-}
-
-bool DBDate::operator>(const DBDate &date) const {
-    return date < *this;
-}
-
-bool DBDate::operator==(const DBDate &date) const {
-    return !(date > *this || date < *this);
-}
-
-bool DBDate::operator<=(const DBDate &date) const {
-    return !(date < *this);
-}
-
-bool DBDate::operator>=(const DBDate &date) const {
-    return !(date > *this);
-}
-
-bool DBDate::operator!=(const DBDate &date) const {
-    return !(*this == date);
-}
-
-int DBDate::DaysInCurYear() const {
-    int x = m_day;
-    for (int i = 1; i < m_month; ++i)
-        x += DBDate::GetDaysInMonth(i, m_year);
-    return x;
-}
-
-int DBDate::operator-(const DBDate &date) const {
-    if (date > *this) return -1;
-
-    int sum = 0;
-
-    int this_days = m_day;
-    for (int i = 1; i < m_month; ++i) {
-        this_days += DBDate::GetDaysInMonth(i, m_year);
+namespace dbmsLib {
+    int DBDate::GetDay() const {
+        return m_day;
     }
 
-    int date_days = date.m_day;
-    for (int i = 1; i < date.m_month; ++i) {
-        date_days += DBDate::GetDaysInMonth(i, date.m_year);
+    int DBDate::GetMonth() const {
+        return m_month;
     }
 
-    int flag = 0;
-
-    if (this_days >= date_days)
-        sum += this_days - date_days;
-    else {
-        flag = 1;
-        sum += (DBDate::IsLeapYear(m_year - flag) ? 366 : 365) - date_days + this_days;
+    int DBDate::GetYear() const {
+        return m_year;
     }
 
-    for (int i = date.m_year; i < m_year - flag; ++i)
-        sum += (DBDate::IsLeapYear(i) ? 366 : 365);
+    DBDate::DBDate(const string &date) {
+        m_day = stoi(date.substr(0, 2));
+        m_month = stoi(date.substr(3, 2));
+        m_year = stoi(date.substr(6, 4));
+    }
 
-    return sum;
-}
+    string DateToStr(const DBDate &date) {
+        return to_string(date.m_day) + to_string(date.m_month) + to_string(date.m_year);
+    }
 
-DBDate &DBDate::operator+=(int days) {
-    m_day +=days;
+    ostream &operator<<(ostream &out, const DBDate &date) {
+        if (date.m_day < 10) out << "0";
+        out << date.m_day << ".";
+        if (date.m_month < 10) out << "0";
+        out << date.m_month << ".";
+        out << date.m_year;
+        return out;
+    }
 
-    while (m_day > DBDate::GetDaysInMonth(m_month, m_year)) {
-        m_day -= DBDate::GetDaysInMonth(m_month, m_year);
-        ++m_month;
-        if (m_month > 12) {
-            ++m_year;
-            m_month = 1;
+    bool DBDate::IsLeapYear(int year) {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
+    }
+
+    int DBDate::GetDaysInMonth(int month, int year) {
+        if (month < 8 && month % 2 == 1) return 31;
+        if (month >= 8 && month % 2 == 0) return 31;
+        if (month == 2) return DBDate::IsLeapYear(year) ? 29 : 28;
+        return 30;
+    }
+
+    bool DBDate::operator<(const DBDate &date) const {
+        if (m_year != date.m_year) return m_year < date.m_year;
+        if (m_month != date.m_month) return m_month < date.m_month;
+        return m_day < date.m_day;
+    }
+
+    bool DBDate::operator>(const DBDate &date) const {
+        return date < *this;
+    }
+
+    bool DBDate::operator==(const DBDate &date) const {
+        return !(date > *this || date < *this);
+    }
+
+    bool DBDate::operator<=(const DBDate &date) const {
+        return !(date < *this);
+    }
+
+    bool DBDate::operator>=(const DBDate &date) const {
+        return !(date > *this);
+    }
+
+    bool DBDate::operator!=(const DBDate &date) const {
+        return !(*this == date);
+    }
+
+    int DBDate::DaysInCurYear() const {
+        int x = m_day;
+        for (int i = 1; i < m_month; ++i)
+            x += DBDate::GetDaysInMonth(i, m_year);
+        return x;
+    }
+
+    int DBDate::operator-(const DBDate &date) const {
+        if (date > *this) return -1;
+
+        int sum = 0;
+
+        int this_days = m_day;
+        for (int i = 1; i < m_month; ++i) {
+            this_days += DBDate::GetDaysInMonth(i, m_year);
         }
-    }
 
-    return *this;
-}
-
-DBDate &DBDate::operator-=(int days) {
-    m_day -= days;
-
-    while (m_day <= 0) {
-        --m_month;
-        if (m_month < 1) {
-            --m_year;
-            m_month = 12;
+        int date_days = date.m_day;
+        for (int i = 1; i < date.m_month; ++i) {
+            date_days += DBDate::GetDaysInMonth(i, date.m_year);
         }
-        m_day += DBDate::GetDaysInMonth(m_month, m_year);
+
+        int flag = 0;
+
+        if (this_days >= date_days)
+            sum += this_days - date_days;
+        else {
+            flag = 1;
+            sum += (DBDate::IsLeapYear(m_year - flag) ? 366 : 365) - date_days + this_days;
+        }
+
+        for (int i = date.m_year; i < m_year - flag; ++i)
+            sum += (DBDate::IsLeapYear(i) ? 366 : 365);
+
+        return sum;
     }
 
-    return *this;
+    DBDate &DBDate::operator+=(int days) {
+        m_day += days;
+
+        while (m_day > DBDate::GetDaysInMonth(m_month, m_year)) {
+            m_day -= DBDate::GetDaysInMonth(m_month, m_year);
+            ++m_month;
+            if (m_month > 12) {
+                ++m_year;
+                m_month = 1;
+            }
+        }
+
+        return *this;
+    }
+
+    DBDate &DBDate::operator-=(int days) {
+        m_day -= days;
+
+        while (m_day <= 0) {
+            --m_month;
+            if (m_month < 1) {
+                --m_year;
+                m_month = 12;
+            }
+            m_day += DBDate::GetDaysInMonth(m_month, m_year);
+        }
+
+        return *this;
+    }
 }
--- dbmsLib.h
+++ dbmsLib.h
@@ -13,34 +13,33 @@ using namespace std;
 
 namespace dbmsLib
 {
-    class DBDate
-    {
+    class DBDate {
+    private:
         static const int arrDays[13];
-        friend string DateToStr(DBDate& date);
-        friend ostream& operator<<(ostream& out,DBDate& date);
-        int day, month, year;
+        friend string DateToStr(const DBDate &date);
+        friend ostream &operator<<(ostream &out, const DBDate &date);
+        int m_day, m_month, m_year;//дата (текущая)
     public:
-        DBDate(string date);
-        DBDate(int d,int m,int y);
-        DBDate():day(0),month(0),year(0){};
-        DBDate(DBDate& dat):day(dat.day),month(dat.month),year(dat.year){}
-        int GetDay();
-        int GetMonth();
-        int GetYear();
-        bool IsLeapYear (int year);
-        int GetDaysInMonth(int month,int year);
-        int DaysInCurYear();
-        bool operator==(DBDate& date);
-        bool operator<(DBDate& date);
-        bool operator>(DBDate& date);
-        bool operator<= (DBDate& date);
-        bool operator>= (DBDate& date);
-        bool operator!= (DBDate& date);
-        DBDate& operator+=(int days);
-        DBDate& operator-=(int days);
-        int operator-(DBDate& date);
+        explicit DBDate(const string &date);//формат строки: dd.mm.yyyy
+        DBDate(int d, int m, int y) : m_day(d), m_month(m), m_year(y) {};
+        DBDate() : m_day(0), m_month(0), m_year(0) {};//конструктор по умолчанию
+        DBDate(const DBDate &dat) = default;
+        int GetDay() const;
+        int GetMonth() const;
+        int GetYear() const;
+        static bool IsLeapYear(int year); //год високосный?
+        static int GetDaysInMonth(int month, int year);//Количество дней в месяце
+        int DaysInCurYear() const;//Количество дней от начала года до текущей даты
+        bool operator==(const DBDate &date) const;
+        bool operator<(const DBDate &date) const;
+        bool operator>(const DBDate &date) const;
+        bool operator<=(const DBDate &date) const;
+        bool operator>=(const DBDate &date) const;
+        bool operator!=(const DBDate &date) const;
+        DBDate &operator+=(int days);//Прибавляет к текущей дате days дней
+        DBDate &operator-=(int days);//Вычитает из текущей даты days дней
+        int operator-(const DBDate &date) const;//Количество дней между текущей датой и date. Если текущая дата < date, результат -1.
     };
-
 
     enum DBType {
         NoType,
